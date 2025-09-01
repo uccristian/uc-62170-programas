d57d5f2af3b2378543721eeee5fad416318040be
cristian vieri macha serva <71917455@continental.edu.pe>
2025-09-01 09:44:55 -0500

    add main

--- src/programasSemana03/herenciaabstractafiguras/Main.java
+++ src/programasSemana03/herenciaabstractafiguras/Main.java
@@ -1,13 +1,34 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package programasSemana03.herenciaabstractafiguras;
 
-/**
- *
- * @author estudiantehyo
- */
+import utils.ConsoleMenu;
+import utils.InputReader;
+
 public class Main {
+    public static void main(String[] args) {
+        String[] opciones = {"Cuadrado", "Circulo", "Salir"};
+        ConsoleMenu menu = new ConsoleMenu(opciones);
+        int opcion;
     
+        do{
+            menu.showMenu("MENU");
+            opcion = menu.selectOption();
+            
+            switch (opcion) {
+                case 1 -> {
+                   int lado = InputReader.readInt("INgrese lado de cuadrado", 1);
+                   Cuadrado cuadrado = new Cuadrado(lado);
+                   double areaCuadrado = cuadrado.area();
+                   System.out.println("El area del cuadrado es: " + areaCuadrado);
+                }
+                case 2 -> {
+                    int radio = InputReader.readInt("Ingrese radio del circulo", 1);
+                    Circulo circulo = new Circulo(radio);
+                    double areaCirculo = circulo.area();
+                    System.out.println("El area del circulo es: " + areaCirculo);
+                }
+                case 3 -> System.out.println("\nGracias...!...Vuelva pronto.");
+            }
+    
+        } while (opcion != opciones.length);
+    }
 }
